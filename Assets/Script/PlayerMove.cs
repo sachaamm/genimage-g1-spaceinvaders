0fb0eea2f2b0b947842e4a4961bf834068988de9
Khyl-git <valentin.macaluso@gmail.com>
2020-12-15 12:08:00 +0100

    BoolLimitWrong

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -19,15 +19,31 @@ public class PlayerMove : MonoBehaviour
        // transform.position = direction + transform.position;
        
         
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow) && isInLimit())
         {
             transform.Translate(Vector3.left * speed);
         }
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow) && isInLimit())
         {
             transform.Translate(Vector3.right * speed);
         }
 
+        
 
     }
+
+     bool isInLimit()
+    {
+        //Screen.width;
+        int width = Screen.width;
+        int minX = -Screen.width/ 2;
+        int maxX = Screen.width/ 2;
+
+        if (minX < transform.position.x && transform.position.x < maxX)
+        {
+            return true;
+        }
+        return false;
+    }
+
 }
