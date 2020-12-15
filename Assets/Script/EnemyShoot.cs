97cada11dc1654de070330e17e57532e0e8b8aa5
hedidjourhi <hedidjourhi@gmail.com>
2020-12-15 17:35:59 +0100

    enemyshoot

---
+++ Assets/Script/EnemyShoot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShoot : MonoBehaviour
+{
+    public GameObject laserPrefab2;
+    float count;
+    public float countMax = 30;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 laserOffset = new Vector3(0, 0.5f, 0);
+        count += Time.deltaTime;
+       
+        if (count > countMax)
+        {
+            GameObject laser2 = Instantiate(laserPrefab2);
+            laser2.transform.position = gameObject.transform.position + laserOffset;
+            count = 0;
+           
+        }
+        
+
+    }
+}
