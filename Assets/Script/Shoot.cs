6da03f8b61189396f2d8f2907d3572c9947fa065
hedidjourhi <hedidjourhi@gmail.com>
2020-12-15 15:13:51 +0100

    laser

---
+++ Assets/Script/Shoot.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+   public GameObject laserPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space)) 
+        {
+            GameObject laser2 = Instantiate(laserPrefab);
+            laser2.transform.position = gameObject.transform.position;
+     }
+    }
+}
