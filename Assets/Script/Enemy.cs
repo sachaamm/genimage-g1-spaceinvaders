bc7660e3867f287203b6cc5f5bb7f60a4de9260e
Ruffi97 <ruffinigeoffrey@outlook.fr>
2020-12-15 16:59:46 +0100

    EnemyMove

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public float speed = 1;
+    bool reachLimit = false;
+    float count;
+    public float countMax = 50;
+    Vector3 direction;
     // Start is called before the first frame update
     void Start()
     {
-        
+        direction = new Vector3(1, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {       
+        if (NextPosIsInLimit(direction * speed))
+        {
+            transform.Translate(direction * speed);
+        }
+        else { reachLimit = true; }
+        if (reachLimit)
+        {
+            count += Time.deltaTime;
+            transform.Translate(Vector3.down * speed);
+            if(count > countMax)
+            {
+                reachLimit = false;
+                count = 0;
+                direction = -direction;
+            }
+        }
         
     }
 
@@ -22,5 +43,17 @@ public class Enemy : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+    bool NextPosIsInLimit(Vector3 direction)
+    {
+        int minX = 0;
+        int maxX = Screen.width;
+        Vector3 nextPos = Camera.main.WorldToScreenPoint(transform.position + direction);
+
+        if (minX < nextPos.x && nextPos.x < maxX)
+        {
+            return true;
+        }
+        return false;
     }
 }
