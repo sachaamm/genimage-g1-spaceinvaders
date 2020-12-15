5daef241c870642285ca9bf85dd9dd6fdecec746
hedidjourhi <hedidjourhi@gmail.com>
2020-12-15 11:32:34 +0100

    moveplayer

---
+++ Assets/Script/PlayerMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    Vector3 direction;
+    public float speed = 0.10f; 
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction = new Vector3(1, 0,0);
+       // transform.position = direction + transform.position;
+        transform.position = direction*speed + transform.position;
+
+    }
+}
